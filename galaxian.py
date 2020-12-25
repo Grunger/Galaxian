a0760fe7291176cea8035c8b4a841a036df325bb
Andrew Rogov <andrewrogov@mail.ru>
2020-12-26 01:58:14 +0500

    Рекорды работают

--- galaxian.py
+++ galaxian.py
@@ -2,6 +2,7 @@ import os
 import pygame
 import pygame_gui
 import sys
+import sqlite3
 
 pygame.init()
 
@@ -10,6 +11,8 @@ WIDTH = 800
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+db = sqlite3.connect(os.path.join('data', 'records.sqlite'))
+cursor = db.cursor()
 
 
 def load_image(name, color_key=None):
@@ -72,7 +75,7 @@ class Enemy(pygame.sprite.Sprite):
     """
 
     def __init__(self, sheet, columns, rows, x, y):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, enemies)
         self.speed = 5
         self.tick = 0
         self.frames = []
@@ -93,6 +96,24 @@ class Enemy(pygame.sprite.Sprite):
         if self.tick == 49:
             self.cur_frame = (self.cur_frame + 1) % 2
         self.image = self.frames[self.cur_frame]
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(all_sprites, player_bullets)
+        self.speed = 5
+        self.image = pygame.Surface((3, 8))
+        self.image.fill(pygame.Color('red'))
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(x, y)
+
+    def update(self):
+        self.rect.y -= self.speed
+        if pygame.sprite.spritecollideany(self, enemies):
+            pygame.sprite.spritecollide(self, enemies, True)
+            self.kill()
+        if self.rect.y < -10:
+            self.kill()
 
 
 def terminate():
@@ -135,7 +156,7 @@ def start_screen():
                     if event.ui_element == start_btn:
                         game()
                     if event.ui_element == records_btn:
-                        pass
+                        show_records()
                     if event.ui_element == exit_btn:
                         terminate()
             manager.process_events(event)
@@ -145,7 +166,7 @@ def start_screen():
         clock.tick(FPS)
 
 
-def new_record():
+def new_record(score):
     """
     Добавление рекорда
     """
@@ -160,19 +181,21 @@ def new_record():
                     name = name[:-1] + chr(event.key).upper() + '|'
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if 300 <= event.pos[0] <= 500 and 200 <= event.pos[1] <= 245:
+                    cursor.execute(f'''insert into records(name, score) VALUES ('{name[:-1]}', {score})''')
+                    db.commit()
                     name = 'RECORDED!'
                     recorded = True
         screen.fill(pygame.Color("black"))
-        font = pygame.font.Font('./data/Amatic-Bold.ttf', 36)
+        font = pygame.font.Font('./data/fonts/20219.ttf', 36)
         button = load_image('button.png')
         screen.blit(button, (300, 200))
-        string_rendered = font.render('Enter your name:', 1, pygame.Color('white'))
+        string_rendered = font.render('Enter your name:', True, pygame.Color('white'))
         intro_rect = string_rendered.get_rect().move(320, 100)
         screen.blit(string_rendered, intro_rect)
-        string_rendered = font.render('OK', 1, pygame.Color('black'))
+        string_rendered = font.render('OK', True, pygame.Color('black'))
         intro_rect = string_rendered.get_rect().move(390, 200)
         screen.blit(string_rendered, intro_rect)
-        string_rendered = font.render(name, 1, pygame.Color('white'))
+        string_rendered = font.render(name, True, pygame.Color('white'))
         intro_rect = string_rendered.get_rect().move(320, 150)
         screen.blit(string_rendered, intro_rect)
         pygame.display.flip()
@@ -183,21 +206,61 @@ def new_record():
             return
 
 
-def game():
-    """
-    Основной игровой цикл
-    """
-    ship = Ship(load_image("ship.gif"), 8, 1, 350, 500)
-    for y in range(20, 300, 50):
-        for x in range(100, 700, 75):
-            enemy = Enemy(load_image('enemy.png'), 2, 1, x, y)
-    running = True
+def show_records():
+    manager = pygame_gui.UIManager((WIDTH, HEIGHT), os.path.join('data', 'menu_theme.json'))
 
+    # Кнопки
+    ok_btn = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((WIDTH // 2 - 75, HEIGHT - 75), (150, 50)),
+        text='Назад',
+        manager=manager
+    )
+    back = load_image('start_fon.jpg')
+    screen.blit(back, (0, 0))
+    y = 100
+    for name, score in sorted(cursor.execute('''select name, score from records''').fetchall(), key=lambda x: -x[1])[:10]:
+        font = pygame.font.Font('./data/fonts/20219.ttf', 36)
+        string_rendered = font.render(f'{name}: {score}', True, pygame.Color('white'))
+        rec_rect = string_rendered.get_rect().move(320, y)
+        y += 50
+        screen.blit(string_rendered, rec_rect)
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 terminate()
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == ok_btn:
+                        start_screen()
+            manager.process_events(event)
+        manager.update(FPS / 1000)
+        manager.draw_ui(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def game():
+    """
+    Основной игровой цикл
+    """
+    ship = Ship(load_image("ship.gif"), 8, 1, 350, 500)
+    #for y in range(20, 300, 50):
+    #    for x in range(100, 700, 75):
+    #        enemy = Enemy(load_image('enemy.png'), 2, 1, x, y)
+    enemy = Enemy(load_image('enemy.png'), 2, 1, 100, 400)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if len(player_bullets.sprites()) < 3:
+                        Bullet(ship.rect.x + ship.rect.w, ship.rect.y)
         ship.cur_anim = 'straight'
         if pygame.key.get_pressed()[pygame.K_LEFT]:
             ship.rect.x -= ship.speed
@@ -210,6 +273,10 @@ def game():
         all_sprites.update()
         pygame.display.flip()
         clock.tick(FPS)
+        score = 40 - len(enemies.sprites())
+        if len(enemies.sprites()) == 0:
+            new_record(score)
+            return 0
     pygame.quit()
 
 
